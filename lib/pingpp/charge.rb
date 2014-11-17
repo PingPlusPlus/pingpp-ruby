791c2387ca90940dee3cf053ae1740b2d5bfb60d
Xufeng Weng <wengxufeng@gmail.com>
2014-11-17 10:22:58 +0800

    remove old refund method

--- lib/pingpp/charge.rb
+++ lib/pingpp/charge.rb
@@ -4,12 +4,6 @@ module Pingpp
     include Pingpp::APIOperations::Create
     include Pingpp::APIOperations::Update
 
-    def refund(params={})
-      response, api_key = Pingpp.request(:post, refund_url, @api_key, params)
-      refresh_from(response, api_key)
-      self
-    end
-
     private
 
     def refund_url
