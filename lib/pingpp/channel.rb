5dcd594c5903961c05fb71d570120d68b9c43297
afon <wengxufeng@gmail.com>
2014-12-20 13:44:03 +0800

    add new channels

--- lib/pingpp/channel.rb
+++ lib/pingpp/channel.rb
@@ -2,8 +2,12 @@ module Pingpp
   class Channel
     ALIPAY = "alipay"
     WECHAT = "wx"
+    WX = "wx"
     UPMP = "upmp"
     ALIPAY_WAP = "alipay_wap"
     UPMP_WAP = "upmp_wap"
+    WX_PUB = "wx_pub"
+    BFB = "bfb"
+    BFB_WAP = "bfb_wap"
   end
 end
