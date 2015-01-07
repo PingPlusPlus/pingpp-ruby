743b6a6701b6f71eebfeb4dbb346d7a26c206d96
afon <wengxufeng@gmail.com>
2015-01-07 11:11:23 +0800

    merge notify into server

--- example/server.rb
+++ example/server.rb
@@ -3,6 +3,7 @@ require 'json'
 require "digest/md5"
 require 'pingpp'
 
+# 创建 charge
 class Pay < WEBrick::HTTPServlet::AbstractServlet
   def do_POST(request, response)
     Pingpp.api_key = "YOUR-KEY"
@@ -62,7 +63,31 @@ class Pay < WEBrick::HTTPServlet::AbstractServlet
   end
 end
 
+# 异步通知
+class Notify < WEBrick::HTTPServlet::AbstractServlet
+  def do_POST(request, response)
+    response_body = 'fail'
+    begin
+      post_data = JSON.parse(request.body)
+      if post_data['object'].nil?
+      elsif post_data['object'] == 'charge'
+        response_body = 'success'
+        # 开发者在此处加入对支付异步通知的处理代码
+      elsif post_data['object'] == 'refund'
+        response_body = 'success'
+        # 开发者在此处加入对退款异步通知的处理代码
+      end
+    rescue JSON::ParserError
+      response_body = 'fail'
+    end
+    response.status = 200
+    response['Content-Type'] = "text/plain"
+    response.body = response_body
+  end
+end
+
 server = WEBrick::HTTPServer.new(:Port => 8000)
 server.mount "/pay", Pay
+server.mount "/notify", Notify
 trap "INT" do server.shutdown end
 server.start
