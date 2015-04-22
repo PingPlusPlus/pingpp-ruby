7a3453f73f6012e207031ae4a2b7f9a1db272a06
afon <wengxufeng@gmail.com>
2015-04-22 14:26:33 +0800

    add wxpub signature generator for js-sdk

--- lib/pingpp/wx_pub_oauth.rb
+++ lib/pingpp/wx_pub_oauth.rb
@@ -1,3 +1,5 @@
+require 'digest/sha1'
+
 module Pingpp
   module WxPubOauth
     def self.create_oauth_url_for_code(app_id, redirect_url, more_info = false)
@@ -13,17 +15,7 @@ module Pingpp
 
     def self.get_openid(app_id, app_secret, code)
       url = create_oauth_url_for_openid(app_id, app_secret, code)
-      request_opts = {
-        :url => url,
-        :verify_ssl => false,
-        :ssl_version => 'TLSv1',
-        :method => 'GET',
-        :headers => false,
-        :open_timeout => 30,
-        :timeout => 30
-      }
-      response = RestClient::Request.execute(request_opts)
-      response = JSON.parse(response.body)
+      response = get_request(url)
       if response['openid'].nil? then
         return nil, response
       else
@@ -40,6 +32,55 @@ module Pingpp
       }
       query_str = Pingpp.uri_encode(query_parts)
       'https://api.weixin.qq.com/sns/oauth2/access_token?' + query_str
+    end
+
+    def self.get_request(url)
+      request_opts = {
+        :url => url,
+        :verify_ssl => false,
+        :ssl_version => 'TLSv1',
+        :method => 'GET',
+        :headers => false,
+        :open_timeout => 30,
+        :timeout => 30
+      }
+      response = RestClient::Request.execute(request_opts)
+      response = JSON.parse(response.body)
+    end
+
+    def self.get_jsapi_ticket(app_id, app_secret)
+      query_parts = {
+        'appid' => app_id,
+        'secret' => app_secret,
+        'grant_type' => 'client_credential'
+      }
+      query_str = Pingpp.uri_encode(query_parts)
+      access_token_url = 'https://api.weixin.qq.com/cgi-bin/token?' + query_str
+      resp = get_request(access_token_url)
+      if !resp['errcode'].nil? then
+        return resp
+      end
+      query_parts = {
+        'access_token' => resp['access_token'],
+        'type' => 'jsapi'
+      }
+      query_str = Pingpp.uri_encode(query_parts)
+      jsapi_ticket_url = 'https://api.weixin.qq.com/cgi-bin/ticket/getticket?' + query_str
+      jsapi_ticket = get_request(jsapi_ticket_url)
+    end
+
+    def self.get_signature(charge, jsapi_ticket, url)
+      if charge['credential'].nil? || charge['credential']['wx_pub'].nil? then
+        return nil
+      end
+      credential = charge['credential']['wx_pub']
+      array_to_sign = [
+        'jsapi_ticket=' + jsapi_ticket,
+        'noncestr=' + credential['nonceStr'],
+        'timestamp=' + credential['timeStamp'],
+        'url=' + url.split('#')[0]
+      ]
+      siganture = Digest::SHA1.hexdigest(array_to_sign.join('&'))
     end
   end
 end
