3ad997365350a1f15b970d1952685d08bece055c
afon <wengxufeng@gmail.com>
2018-01-05 17:04:51 +0800

    remove transfer cancel

--- lib/pingpp/batch_transfer.rb
+++ lib/pingpp/batch_transfer.rb
@@ -2,7 +2,6 @@ module Pingpp
   class BatchTransfer < APIResource
     extend Pingpp::APIOperations::Create
     extend Pingpp::APIOperations::List
-    include Pingpp::APIOperations::Update
 
     def self.object_name
       'batch_transfer'
@@ -10,10 +9,6 @@ module Pingpp
 
     def self.resource_url(opts={})
       '/v1/batch_transfers'
-    end
-
-    def self.cancel(id, opts={})
-      update(id, {:status => 'canceled'}, opts)
     end
   end
 end
--- lib/pingpp/transfer.rb
+++ lib/pingpp/transfer.rb
@@ -2,14 +2,9 @@ module Pingpp
   class Transfer < APIResource
     extend Pingpp::APIOperations::Create
     extend Pingpp::APIOperations::List
-    include Pingpp::APIOperations::Update
 
     def self.resource_url(opts={})
       '/v1/transfers'
-    end
-
-    def self.cancel(id, opts={})
-      update(id, {:status => 'canceled'}, opts)
     end
 
   end
